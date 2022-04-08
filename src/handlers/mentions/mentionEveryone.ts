d2c99cf3c9ba8ac32ac2bb9cc223f4814251f2f2
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2022-04-08 15:48:47 +0700

    refactor mention everyone

--- src/handlers/mentions/mentionEveryone.ts
+++ src/handlers/mentions/mentionEveryone.ts
@@ -1,29 +1,28 @@
 import { proto } from "@adiwajshing/baileys";
+import logger from "../../logger";
 import sock from "../../sock";
 import { ResolverFunction, ResolverFunctionCarry, ResolverResult } from "../../types/resolver";
 
 
 const mentionEveryone: ResolverFunctionCarry = (): ResolverFunction => async (message: proto.IWebMessageInfo, jid: string, isFromGroup: Boolean): Promise<ResolverResult> => {
-    if (!isFromGroup) {
-        return {
-            destinationId: jid,
-            message: { text: "You can only use this feature in a group chat" },
-            options: {
-                quoted: message,
-            }
-        };
-    }
-    const participantsJids = (await sock.groupMetadata(jid)).participants.map((p) => p.id) ?? [];
-    return {
+    const result: ResolverResult = {
         destinationId: jid,
-        message: {
-            text: '.',
-            mentions: participantsJids,
-        },
         options: {
             quoted: message,
-        }
+        },
+    };
+    if (!isFromGroup) {
+        result.message = { text: "You can only use this feature in a group chat" };
+        return result;
     }
+    const participantsJids = (await sock.groupMetadata(jid)).participants.map((p) => p.id) ?? [];
+    logger.info('participants jids');
+    logger.info(participantsJids);
+    result.message = {
+        text: '.',
+        mentions: participantsJids,
+    }
+    return result;
 }
 
 export default mentionEveryone;
