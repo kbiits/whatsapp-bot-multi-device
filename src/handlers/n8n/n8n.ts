ed1b12a642cdad7c0337dcde1bd9b176c0d36fc8
Nabiel Omar Syarif <nabiel.syarif@advancegroup.com>
2025-06-15 23:15:35 +0700

    remove dev number filter for n8n

--- src/handlers/n8n/n8n.ts
+++ src/handlers/n8n/n8n.ts
@@ -2,6 +2,7 @@ import { proto } from "baileys";
 import { ResolverFunctionCarry, ResolverResult } from "../../types/resolver";
 import getSenderPhone from "../../utils/getSender";
 import axios from "axios";
+import logger from "../../logger";
 
 const developerNumbers = JSON.parse(process.env.DEVELOPER_WHATSAPP_NUMBER);
 const webhookUrl = process.env.N8N_WEBHOOK_URL;
@@ -17,26 +18,30 @@ const sendN8nWebhook: ResolverFunctionCarry = (matches) => async (message: proto
         return { message: { text: 'Sender phone number could not be determined.' }, destinationId: jid };
     }
 
-    if (!developerNumbers.includes(senderPhone)) {
-        console.warn('Webhook not sent for developer number:', senderPhone);
-        return null;
-    }
+    // if (!developerNumbers.includes(senderPhone)) {
+    //     console.warn('Webhook not sent for developer number:', senderPhone);
+    //     return null;
+    // }
 
     const payload = {
         message: message.message.conversation || message.message.extendedTextMessage?.text || '',
         sender: participant,
     };
 
-    const resp = await axios.post(webhookUrl, payload, {
-        auth: {
-            username: 'nabiel',
-            password: 'nabiel',
-        },
-    })
+    try {
+        const resp = await axios.post(webhookUrl, payload, {
+            auth: {
+                username: 'nabiel',
+                password: 'nabiel',
+            },
+        })
 
-    return {
-        destinationId: jid,
-        message: { text: resp.data.text },
+        return {
+            destinationId: jid,
+            message: { text: resp.data.text },
+        }
+    } catch (error) {
+        logger.error('Error sending webhook to n8n:', error);
     }
 }
 
