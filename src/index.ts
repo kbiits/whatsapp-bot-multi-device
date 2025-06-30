1f4cf734971cbd1a2aff3a001d3948ea27d91850
Nabiel Omar Syarif <nabiel.syarif@advancegroup.com>
2025-06-30 22:41:19 +0700

    start the API

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,70 @@ import 'dotenv/config'
 import mongoose from 'mongoose';
 import logger from './logger';
 import sock from './sock';
+import bootstrapAPI from './api';
 
 const DB_URI = process.env.MONGODB_URI || null;
 if (!DB_URI) {
     throw Error('DB URI Not Found!');
 }
 
+const api = bootstrapAPI();
+
 // listen to sigterm/sigint signal
-const gracefulShutdown = async () => {
+const shutdown = async () => {
     logger.warn('Received shutdown signal, closing connections...');
 
-    if (!sock.socket) return;
+    if (api.getServer()) {
+        const promiseStopAPI = new Promise((res) => {
+            api.stop(() => {
+                res(true);
+            });
+        })
+        logger.info('Stopping API server...');
+        await promiseStopAPI;
+        logger.info('API server stopped');
+    }
 
-    if (!sock.socket.ws.isOpen) return;
+    if (sock.socket && sock.socket.ws && sock.socket.ws.isOpen) {
+        logger.info('Closing WhatsApp connection...');
+        await sock.socket.ws.close();
+        logger.info('WhatsApp connection closed');
+    }
 
-    await sock.socket.ws.close();
-    await mongoose.disconnect();
-    logger.info('App shut down gracefully');
+    if (mongoose.connection.readyState === mongoose.ConnectionStates.connected) { // only disconnect if the connection is open
+        logger.info('Disconnecting from database...');
+        await mongoose.disconnect();
+        logger.info('Database disconnected');
+    }
+};
+
+const gracefullyShutdown = async () => {
+    await shutdown();
+    logger.info('App is gracefully shutdown');
     process.exit(0);
 };
 
-process.on("SIGINT", gracefulShutdown);
-process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefullyShutdown);
+process.on("SIGTERM", gracefullyShutdown);
 
 (async () => {
-    console.log("Connecting to db...");
-    mongoose
-        .connect(DB_URI)
-        .then(async () => {
-            console.log("db connected");
+    try {
+        // connect to the database
+        logger.info("Connecting to db...");
+        const mongooseConnection = await mongoose.connect(DB_URI);
+        logger.info("db connected");
 
-            // start whatsapp connection
-            const Socket = await import("./sock")
-            await Socket.default.connect()
-            logger.warn('Started WhatsApp connection...');
-        })
-        .catch((err) => {
-            console.log('Failed to connect to db');
-            console.log(err);
-            throw err;
-        });
-})().catch((err) => console.log('encountered error : ', err))
+        // start whatsapp connection
+        await sock.connect()
+        logger.info('Started WhatsApp connection...');
+
+        // start the API
+        api.start();
+        logger.info(`API server started on port ${process.env.HTTP_PORT || 3000}`);
+    } catch (error) {
+        logger.error("Error starting the app: ", error);
+        shutdown();
+        logger.error("App shutdown due to error");
+        process.exit(1);
+    }
+})()
