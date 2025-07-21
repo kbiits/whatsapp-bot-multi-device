5da26056ced7616134beef9f9e2acdba203b8a11
Nabiel Omar Syarif <nabiel.syarif@advancegroup.com>
2025-07-21 19:00:25 +0700

    fix log error

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,31 @@
 import pino from "pino";
 import PinoPretty from "pino-pretty";
+import colorette from "colorette";
 
 const ENV = process.env.ENVIRONMENT?.toLowerCase() || 'production';
 const logLevel = process.env.LOGGER_LEVEL || (ENV == 'production' ? 'warn' : 'debug');
 
 const prettier = PinoPretty({
     messageFormat: '{filename} - {msg}',
-    colorize: false,
+    colorize: true,
     translateTime: 'SYS:standard',
 })
 
 const logger = pino({
-    level: logLevel,
-}, prettier);
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: colorette.isColorSupported,
+            colorizeObjects: true,
+            crlf: false,
+            errorLikeObjectKeys: ['err', 'error'],
+            levelFirst: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+            messageFormat: '{filename} - {msg}',
+
+        },
+    }
+});
 
 export default logger;
